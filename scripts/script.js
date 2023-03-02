b9f9851937f8fe2d02fccbc3f7b4212c7fb876fd
moktubat <shadhinreja@gmail.com>
2023-03-02 23:08:48 +0600

    added bt and js link with html

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const fetchData = () => {
+    const url = 'https://openapi.programming-hero.com/api/ai/tools';
+    fetch (url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
