0c360bc2f811d5929d3ad91beeec464509488091
moktubat <shadhinreja@gmail.com>
2023-03-03 02:52:40 +0600

    added card img, discription, icon

--- scripts/script.js
+++ scripts/script.js
@@ -7,22 +7,42 @@ const fetchCard = () => {
 
 const displayCards = cards => {
     const cardsContainer = document.getElementById('cards-container');
+    cards.tools = cards.tools.slice(0, 6);
     cards.tools.forEach(card => {
         const cardDiv = document.createElement('div');
-        cardDiv.classList.add('col');
+        cardDiv.classList.add('col', 'pb-4');
         cardDiv.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
+            <img src="${card.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Features</h5>
+                <ol>
+                <li class="card-text">${card.features[0] ? card.features[0] : ''}</li>
+                <li class="card-text">${card.features[1] ? card.features[1] : ''}</li>
+                <li class="card-text">${card.features[2] ? card.features[2] : ''}</li>
+                <li class="card-text">${card.features[3] ? card.features[3] : ''}</li>
+                </ol>
+            </div>
+            <div class="card-footer border-0 bg-body">
+                <div class="row">
+                  <div class="col">
+                    <h5 class="card-title">${card.name}</h5>
+                    <p class="card-text"><i class="fa-regular fa-calendar-days"></i> <small class="text-muted">${card.published_in}</small></p>
+                  </div>
+
+                  <div class="col align-self-end">
+                  <button type="button" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
+                  
+                  </div>
+                </div>
+            </div>
+                
+            
+
         </div>
         `;
         cardsContainer.appendChild(cardDiv);
+
     });
 }
 
